4030dbc2f12a7286f329b3024feb52692d4f128f
Sophie Goldstein <110262769+Sophgolds@users.noreply.github.com>
2023-02-05 18:45:40 -0500

    Update server.js

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,21 +1,19 @@
-const { PORT = 5000 } = process.env;
+const express = require('express');
+const cors = require('cors');
 
-const app = require("./app");
-const knex = require("./db/connection");
+const app = express();
+const router = express.Router();
+const PORT = process.env.PORT || 5001;
 
-knex.migrate
-  .latest()
-  .then((migrations) => {
-    console.log("migrations", migrations);
-    app.listen(PORT, listener);
-  })
-  .catch((error) => {
-    console.error(error);
-    knex.destroy();
-  });
+router.get('/', cors(), (req, res) => {
+  res.json({ message: 'Hello Render!' });
+});
 
-function listener() {
-  console.log(`Listening on Port ${PORT}!`);
-}
+app.use('/', router);
 
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT} `);
+});
+
+module.exports = app;
 
