ea7d786d567615d7758fbf9c4d3cbbd85d77c306
Sophie Goldstein <110262769+Sophgolds@users.noreply.github.com>
2023-05-08 17:02:09 -0400

    Update 01-tables.js

--- back-end/src/db/seeds/01-tables.js
+++ back-end/src/db/seeds/01-tables.js
@@ -1,9 +1,10 @@
 const table = require("./01-tables.json")
 
 exports.seed = function (knex) {
-  return knex.raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE").then(() => {
-    return knex("tables").insert(table)
-  }
- }
-                                                                         
+  return knex
+    .raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
+    .then(function () {
+      return knex("tables").insert(tables);
+    })
+};                                                                     
      
